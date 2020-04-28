1f7196b21bc7adfe7f0d7ba8e43d399d258baa36
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-04-28 22:22:37 +0200

    added buttons for mobile menu

---
+++ javascript/mobile_menu.js
@@ -0,0 +1,9 @@
+var menu = document.querySelector("#mobile_menu");
+
+function hidemenu(){
+    menu.style.transform = "translateX(100%)";
+}
+
+function showmenu(){
+    menu.style.transform = "translateX(0%)";
+}
